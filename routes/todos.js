a700b67b7f490caad1291bbe6530ec88daa16fd1
Mohit Kumar <mhtkmr54@gmail.com>
2015-06-15 19:56:46 +0530

    minor changes

--- routes/todos.js
+++ routes/todos.js
@@ -20,6 +20,28 @@ router.post('/', function(req, res, next) {
   });
 });
 
+/* GET /todos/id */
+router.get('/:id', function(req, res, next) {
+  Todo.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
 
+/* PUT /todos/:id */
+router.put('/:id', function(req, res, next) {
+  Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE /todos/:id */
+router.delete('/:id', function(req, res, next) {
+  Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
 
 module.exports = router;
